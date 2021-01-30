51966837c5844b3a8b248c677a38841f57663f03
Juan carlos <juancarloscruz278@gmail.com>
2021-01-30 09:40:37 -0400

    fix: refactor Apssjs content with the corresponding components.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,22 @@
-import { Header } from "./components/Header/Header";
 import phoneImage from "./assets/HoldingPhone.webp";
 import okLaggom from "./assets/OkLaggom.png";
 import Lagom from "./assets/Lagggom.png";
-import { Links } from "./components/Links";
+import { HeaderContent } from "./components/Header/HeaderContent";
+import { SocialContent } from "./components/Social/SocialContent";
 
 function App() {
   return (
     <>
       <div className="grid-content animation-fade-in">
-        <div className="space-content header__content">
-          <Header />
-          <div className="header__title__container">
-            <h1 className="header__content__title animation-fade-up">Digital products lovingly made with a human touch.</h1>
-          </div>
-        </div>
+       <HeaderContent/>
         <img src={phoneImage} alt="phone user" className="header__content__img animation-fade-in" />
       </div>
       <main>
         <div className="grid-content">
-          <div className="space-social-content-X socials__content">
-            <Links url="https://dribbble.com" />
-            <div className="spacer"></div>
-            <Links titleName="Twitter" hasTitleColor url="https://twitter.com" />
-          </div>
+         <SocialContent/>
           <div className="images__content">
-            <img src={okLaggom} alt="Laggom Logo" loading="lazy" className="animation-fade-in" />
-            <img src={Lagom} alt="Lagom office" loading="lazy" className="animation-fade-in" />
+            <img src={okLaggom} alt="Laggom Logo" className="animation-fade-in" />
+            <img src={Lagom} alt="Lagom office" className="animation-fade-in" />
           </div>
         </div>
       </main>
